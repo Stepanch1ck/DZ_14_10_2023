f33ba8d4d60aaecec59d95ab7a43260dfbe1faa6
Stepan Ch <noname.churkin@gmail.com>
2023-10-19 17:34:06 +0300

    Добавьте файлы проекта.

---
+++ Ski_Resort/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Ski_Resort
+{
+    abstract class SkiResort
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int NumberofTrails { get; set; }
+
+        public abstract void CalculateLiftTicketPrice();
+        public abstract void CalculateAccommodationPrice();
+
+        public SkiResort(string name, string location, int numberofTrails)
+        {
+            Name = name;
+            Location = location;
+            NumberofTrails = numberofTrails;
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {Name}, расположение: {Location}, количество трасс: {NumberofTrails}";
+        }
+    }
+    /// <summary>
+    /// класс с описание отеля FamilySkiResort
+    /// </summary>
+    class FamilySkiResort : SkiResort
+    {
+        public int NumberofChildcareCenters { get; set; }
+
+        public FamilySkiResort(string name, string location, int numberofTrails, int numberofChildcareCenters)
+            : base(name, location, numberofTrails)
+        {
+            NumberofChildcareCenters = numberofChildcareCenters;
+        }
+        /// <summary>
+        /// Расчёт стоимости абонемента
+        /// </summary>
+        public override void CalculateLiftTicketPrice()
+        {
+            double familyLiftTicketPrice = 5000 * NumberofTrails;
+            double childLiftTicketPrice = 2500 * NumberofTrails;
+            double adultLiftTicketPrice = 3500 * NumberofTrails;
+            if (NumberofChildcareCenters > 0)
+            {
+                familyLiftTicketPrice = familyLiftTicketPrice * 0.9;
+            }
+            double totalLiftTicketPrice = familyLiftTicketPrice + childLiftTicketPrice + adultLiftTicketPrice;
+
+            Console.WriteLine($"Стоимость абонемента на сезон для семьи: {totalLiftTicketPrice} рублей");
+        }
+        /// <summary>
+        /// Расчёт стоимости проживания
+        /// </summary>
+        public override void CalculateAccommodationPrice()
+        {
+            int doubleRoomPrice = 10000 * NumberofTrails;
+            int familyRoomPrice = 20000 * NumberofTrails;
+            int totalAccommodationPrice = doubleRoomPrice + familyRoomPrice;
+
+            Console.WriteLine($"Стоимость проживания на сезон: {totalAccommodationPrice} рублей");
+        }
+
+    }
+    /// <summary>
+    /// класс с описание отеля LuxurySkiResort
+    /// </summary>
+    class LuxurySkiResort : SkiResort
+    {
+        public int NumberofChalets { get; set; }
+
+        public LuxurySkiResort(string name, string location, int numberofTrails, int numberofChalets)
+            : base(name, location, numberofTrails)
+        {
+            NumberofChalets = numberofChalets;
+        }
+        /// <summary>
+        /// расчёт стоимости абонемента
+        /// </summary>
+        public override void CalculateLiftTicketPrice()
+        {
+            int adultLiftTicketPrice = 4500 * NumberofTrails;
+            int childLiftTicketPrice = 3000 * NumberofTrails;
+            int totalLiftTicketPrice = adultLiftTicketPrice + childLiftTicketPrice;
+
+            Console.WriteLine($"Стоимость абонемента на сезон: {totalLiftTicketPrice} рублей");
+        }
+        /// <summary>
+        /// расчёт стоимости проживания
+        /// </summary>
+        public override void CalculateAccommodationPrice()
+        {
+            int hotelPrice = 15000 * NumberofTrails;
+            int chaletPrice = 25000 * NumberofTrails;
+            int totalAccommodationPrice = hotelPrice + chaletPrice;
+
+            Console.WriteLine($"Стоимость проживания на сезон: {totalAccommodationPrice} рублей");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тема: Лыжный курорт, реализовать несколько классов, методов, свойств.");
+            FamilySkiResort familySkiResort = new FamilySkiResort("Красная Поляна", "Россия", 100, 2);
+            Console.WriteLine(familySkiResort);
+            familySkiResort.CalculateLiftTicketPrice();
+            familySkiResort.CalculateAccommodationPrice();
+            LuxurySkiResort luxurySkiResort = new LuxurySkiResort("Альпы", "Австрия", 200, 10);
+            Console.WriteLine(luxurySkiResort);
+            luxurySkiResort.CalculateLiftTicketPrice();
+            luxurySkiResort.CalculateAccommodationPrice();
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tumakov_Lab7/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace Tumakov_Lab7
+{
+    public class Building
+    {
+        private int uniqueNumber;
+        private int Height;
+        private int Floors;
+        private int Apartments;
+        private int Entrances;
+
+        public Building(int height, int floors, int apartments, int entrances)
+        {
+            Height = height;
+            Floors = floors;
+            Apartments = apartments;
+            Entrances = entrances;
+            uniqueNumber = getNextUniqueNumber();
+        }
+        private static int nextUniqueNumber = 1;
+
+        public int GetUniqueNumber()
+        {
+            return uniqueNumber;
+        }
+
+        public int GetHeight()
+        {
+            return Height;
+        }
+
+        public int GetFloors()
+        {
+            return Floors;
+        }
+
+        public int GetApartments()
+        {
+            return Apartments;
+        }
+
+        public int GetEntrances()
+        {
+            return Entrances;
+        }
+
+        public int GetApartmentsOnFloor()
+        {
+            return Apartments / Floors;
+        }
+
+        public int GetApartmentsInEntrance()
+        {
+            return Apartments / Entrances;
+        }
+
+        public int GetFloorHeight()
+        {
+            return Height / Floors;
+        }
+        private static int getNextUniqueNumber()
+        {
+            int uniqueNumber = nextUniqueNumber;
+            nextUniqueNumber++;
+            return uniqueNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Номер здания: {uniqueNumber}, высота: {Height}, этажность: {Floors}, количество квартир: {Apartments}, количество подъездов: {Entrances}";
+        }
+    }
+
+    public class BankAccount
+    {
+        protected int AccountNumber;
+        protected decimal Balance;
+        protected AccountType AccountType;
+
+        public BankAccount(int accountNumber, decimal balance, AccountType accountType)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            AccountType = accountType;
+        }
+
+        public int GetAccountNumber()
+        {
+            return AccountNumber;
+        }
+
+        public void SetAccountNumber(int accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public decimal GetBalance()
+        {
+            return Balance;
+        }
+
+        public void SetBalance(decimal balance)
+        {
+            Balance = balance;
+        }
+
+        public AccountType GetAccountType()
+        {
+            return AccountType;
+        }
+
+        public void SetAccountType(AccountType accountType)
+        {
+            AccountType = accountType;
+        }
+
+        public override string ToString()
+        {
+            return $"Номер счета: {AccountNumber}, баланс: {Balance}, тип счета: {AccountType}";
+        }
+        /// <summary>
+        /// снятие со счёта с средств, если это возможно
+        /// </summary>
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                throw new ArgumentException("Недостаточно средств");
+            }
+
+            Balance -= amount;
+        }
+        /// <summary>
+        /// Пополнение счёта
+        /// </summary>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+    }
+
+    public enum AccountType
+    {
+        Current,
+        Saving
+    }
+    /// <summary>
+    /// генерация различных номеров для аккаунтов
+    /// </summary>
+    public class UniqueBankAccount : BankAccount
+    {
+        private static int nextAccountNumber = 1;
+
+        public UniqueBankAccount(decimal balance, AccountType accountType)
+            : base(GenerateAccountNumber(), balance, accountType)
+        {
+        }
+
+        private static int GenerateAccountNumber()
+        {
+            int accountNumber = nextAccountNumber;
+            nextAccountNumber++;
+            return accountNumber;
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 7.1: Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип\r\nбанковского счета");
+            BankAccount account = new BankAccount(1234567890, 10000, AccountType.Current);
+            Console.WriteLine(account);
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 7.2: Изменить класс счет в банке из упражнения 7.1 таким образом, чтобы\r\nномер счета генерировался сам и был уникальным.");
+            UniqueBankAccount account1 = new UniqueBankAccount(10000, AccountType.Current);
+            UniqueBankAccount account2 = new UniqueBankAccount(5000, AccountType.Saving);
+            Console.WriteLine(account1);
+            Console.WriteLine(account2);
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Упражнение 7.3: Реализовать снятие и пополнение счёта");
+            Console.WriteLine("Снимаем 5000 со счёта пользователя Account1");
+            account1.Withdraw(5000);
+            Console.WriteLine(account1);
+            Console.WriteLine("Поподняем на 1000 счёт пользователя Account2");
+            account2.Deposit(1000);
+            Console.WriteLine(account2);
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+
+            Console.WriteLine("Домашнее задание 7.1: Реализовать класс для описания здания (уникальный номер здания,\r\nвысота, этажность, количество квартир, подъездов).");
+            Building building = new Building(10, 10, 100, 5);
+            Console.WriteLine(building);
+            Console.WriteLine("Количество квартир на этаже: " + building.GetApartmentsOnFloor());
+            Console.WriteLine("Количество квартир в подъезде: " + building.GetApartmentsInEntrance());
+            Console.WriteLine("Высота этажа: " + building.GetFloorHeight());
+            Building building2 = new Building(15, 5, 120, 10);
+            Console.WriteLine(building2);
+            Console.WriteLine("Количество квартир на этаже: " + building2.GetApartmentsOnFloor());
+            Console.WriteLine("Количество квартир в подъезде: " + building2.GetApartmentsInEntrance());
+            Console.WriteLine("Высота этажа: " + building2.GetFloorHeight());
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+        }
+    }
+
+}
